3da99420691a113488b6f2a686b205e2952c7a9d
Carl Mueller <cmuell89@gmail.com>
2018-01-31 19:40:22 -0700

    updating is to ==, debug print statements

--- predicate_classification/src/predicate_classification/predicate_classifiers.py
+++ predicate_classification/src/predicate_classification/predicate_classifiers.py
@@ -26,28 +26,28 @@ def get_euclidean_distance(x1, y1, z1, x2, y2, z2):
 
 def upright(upright_pose, current_pose, threshold_angle, axis="z"):
 
-    if axis is "x":
-        ref_vec = np.array([1., 0., 0.]) # Unit vector in the +x direction
-    if axis is "y":
-        ref_vec = np.array([0., 1., 0.]) # Unit vector in the +y direction
-    if axis is "z":
-        ref_vec = np.array([0., 0., 1.]) # Unit vector in the +z direction
+    if axis == "x":
+        ref_vec = np.array([1., 0., 0.])  # Unit vector in the +x direction
+    elif axis == "y":
+        ref_vec = np.array([0., 1., 0.])  # Unit vector in the +y direction
+    else:
+        ref_vec = np.array([0., 0., 1.])  # Unit vector in the +z direction
     upright_q = Quaternion(upright_pose.orientation.w,
-                           upright_pose.orientation.x, 
-                           upright_pose.orientation.y, 
+                           upright_pose.orientation.x,
+                           upright_pose.orientation.y,
                            upright_pose.orientation.z)
 
     current_q = Quaternion(current_pose.orientation.w,
-                           current_pose.orientation.x, 
-                           current_pose.orientation.y, 
+                           current_pose.orientation.x,
+                           current_pose.orientation.y,
                            current_pose.orientation.z)
 
     upright_vec = upright_q.rotate(ref_vec)
     current_vec = current_q.rotate(ref_vec)
     angle = np.rad2deg(angle_between(upright_vec, current_vec))
 
-    rospy.loginfo("Angle of devation from upright pose to current pose: {}".format(angle))
-    print(angle)
+    rospy.logdebug("Angle of devation from upright pose to current pose: {}".format(angle))
+
     # Classification
     if angle < threshold_angle:
         return 1
@@ -66,7 +66,7 @@ def proximity(object1_pose, object2_pose, threshold_distance):
 
     distance = get_euclidean_distance(o1_x, o1_y, o1_z, o2_x, o2_y, o2_z)
 
-    rospy.loginfo("Distance between objects: {}".format(distance))
+    rospy.logdebug("Distance between objects: {}".format(distance))
 
     if distance < threshold_distance:
         return 1
@@ -79,11 +79,9 @@ def height(object_pose, reference_height, threshold_distance):
 
     difference = fabs(object_height - reference_height)
 
-    rospy.loginfo("Height differential: {}".format(difference))
+    rospy.logdebug("Height differential: {}".format(difference))
 
     if difference >= threshold_distance:
         return 1
     else:
         return 0
-
-    
