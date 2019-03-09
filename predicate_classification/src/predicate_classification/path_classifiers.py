53686d161083f1c9ccf5496b39d95b761a7a72b4
Micah-Zhang <micah.zhang@colorado.edu>
2019-03-08 18:20:28 -0700

    added perimeter constraint

--- predicate_classification/src/predicate_classification/path_classifiers.py
+++ predicate_classification/src/predicate_classification/path_classifiers.py
@@ -1,4 +1,17 @@
-
+from predicate_classification.utils import pt_in_polygon
 
 def perimeter(pose, inner_polygon, outer_polygon, axis="z"):
-    pass
+	if axis == "z":
+		p1 = pose.position.x
+    	p2 = pose.position.y
+	elif axis == "y":
+		p1 = pose.position.x
+    	p2 = pose.position.z
+	elif axis == "x":
+		p1 = pose.position.y
+		p2 = pose.position.z
+    
+    if (pt_in_polygon(p1,p2,outer_polygon) is True) and (pt_in_polygon(p1,p2,inner_polygon) is False):
+			return 1
+		else:
+			return 0
