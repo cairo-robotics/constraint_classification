1eb2c053882cf3261e2b00e27af46b095d5e81c3
Micah-Zhang <micah.zhang@colorado.edu>
2019-03-14 16:19:25 -0600

    added documentation for perimeter function and related tests

--- predicate_classification/src/predicate_classification/path_classifiers.py
+++ predicate_classification/src/predicate_classification/path_classifiers.py
@@ -1,6 +1,25 @@
 from predicate_classification.utils import pt_in_polygon
 
 def perimeter(pose, inner_polygon, outer_polygon, axis="z"):
+	"""
+    Given coordinates of closed 2D polygon with a hole in the center, determines whether set of 2D coordinates is within polygon boundaries
+    
+    Parameters
+    ----------
+    pose : geometry_msgs/Pose
+        Pose object with 2D coordinates 
+    inner_polygon : list
+    	List of tuples (x,y) specifiying inner boundary of polygon
+    outer_polygon: list
+    	List of tuples (x,y) specifiying outer boundary of polygon
+    axis : str
+        Axis against which to measure deviation.
+
+    Returns
+    -------
+    : int
+        1 if pose is inside polygon, 0 otherwise.
+    """
 	if axis == "z":
 		p1 = pose.position.x
 		p2 = pose.position.y
--- predicate_classification/src/test/test_path_classifiers.py
+++ predicate_classification/src/test/test_path_classifiers.py
@@ -9,6 +9,22 @@ from predicate_classification.path_classifiers import perimeter
 class TestPerimeterConstraint(unittest.TestCase):
 
 	def test_square_perimeter(self):
+
+		"""
+    	Defines close polygon with hole in the center with inner and outer boundaries as squares.
+    	Calls the "perimeter" function with 3 different coordinates, one outside the outer bounary, 
+    	one between the inner and outer boundaries, and one inside the inner boundary,
+    	and checks that the "perimeter" function returns correct values.
+
+   		Parameters
+    	----------
+    	None.
+
+    	Returns
+    	-------
+    	: None.
+    	"""
+
 		outer_polygon = [[-8,-8],[-8,8],[8,8],[8,-8]]
 		inner_polygon = [[-4,-4],[-4,4],[4,4],[4,-4]]
 
@@ -49,6 +65,22 @@ class TestPerimeterConstraint(unittest.TestCase):
 		self.assertEquals(result3, 0)
 
 	def test_rectangle_perimeter(self):
+
+		"""
+    	Defines close polygon with hole in the center with inner and outer boundaries as rectangles.
+    	Calls the "perimeter" function with 3 different coordinates, one outside the outer bounary, 
+    	one between the inner and outer boundaries, and one inside the inner boundary,
+    	and checks that the "perimeter" function returns correct values.
+
+   		Parameters
+    	----------
+    	None.
+
+    	Returns
+    	-------
+    	: None.
+    	"""
+
 		outer_polygon = [[-8,-8],[-8,8],[16,8],[16,-8]]
 		inner_polygon = [[-4,-4],[-4,4],[12,4],[12,-4]]
 
@@ -88,7 +120,24 @@ class TestPerimeterConstraint(unittest.TestCase):
 		result3 = perimeter(pose3, inner_polygon, outer_polygon)
 		self.assertEquals(result3, 0)
 
+
 	def test_circle_perimeter(self):
+
+		"""
+    	Defines close polygon with hole in the center with inner and outer boundaries as circles.
+    	Calls the "perimeter" function with 3 different coordinates, one outside the outer bounary, 
+    	one between the inner and outer boundaries, and one inside the inner boundary,
+    	and checks that the "perimeter" function returns correct values.
+
+   		Parameters
+    	----------
+    	None.
+
+    	Returns
+    	-------
+    	: None.
+    	"""
+
 		outer_polygon = [[0,-4],[-4,0],[0,4],[4,0]]
 		inner_polygon = [[0,-2],[-2,0],[0,2],[2,0]]
 
@@ -129,6 +178,22 @@ class TestPerimeterConstraint(unittest.TestCase):
 		self.assertEquals(result3, 0)
 
 	def test_triangle_perimeter(self):
+
+		"""
+    	Defines close polygon with hole in the center with inner and outer boundaries as triangles.
+    	Calls the "perimeter" function with 3 different coordinates, one outside the outer bounary, 
+    	one between the inner and outer boundaries, and one inside the inner boundary,
+    	and checks that the "perimeter" function returns correct values.
+
+   		Parameters
+    	----------
+    	None.
+
+    	Returns
+    	-------
+    	: None.
+    	"""
+
 		outer_polygon = [[-6,-2],[0,5],[6,-2]]
 		inner_polygon = [[-3,-1],[0,3],[3,-1]]
 
@@ -169,6 +234,22 @@ class TestPerimeterConstraint(unittest.TestCase):
 		self.assertEquals(result3, 0)
 
 	def test_star_perimeter(self):
+
+		"""
+    	Defines close polygon with hole in the center with inner and outer boundaries as 5-point stars.
+    	Calls the "perimeter" function with 3 different coordinates, one outside the outer bounary, 
+    	one between the inner and outer boundaries, and one inside the inner boundary,
+    	and checks that the "perimeter" function returns correct values.
+
+   		Parameters
+    	----------
+    	None.
+
+    	Returns
+    	-------
+    	: None.
+    	"""
+
 		outer_polygon = [[-10,-10],[-5,0],[-14,8],[-4,8],[0,14],[4,8],[14,8],[6,0],[10,-10],[0,-6]]
 		inner_polygon = [[-2,0],[-1,1],[-2,2],[-1,2],[0,3],[1,2],[2,2],[1,1],[2,0],[0,1]]
 
